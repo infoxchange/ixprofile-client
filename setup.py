1f586941d0ef64f2930053b25f104758f0ecae9f
Alexey Kotlyarov <alexey@infoxchange.net.au>
2013-05-13 11:47:46 +1000

    Change from flake8 to pep8 and pylint-mccabe

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages
 setup(
     name='IXProfileClient',
     version='0.1.0',
-    author='Infoxchanhe Australia dev team',
+    author='Infoxchange Australia dev team',
     author_email='devs@infoxchange.net.au',
     packages=find_packages(),
     license='MIT',
@@ -16,8 +16,9 @@ setup(
     install_requires=[
         "Django >= 1.4.0",
         "django-social-auth >= 0.7.20",
-        "flake8 >= 2.0",
+        "pep8 >= 1.4.5",
         "pylint >= 0.27.0",
+        "pylint-mccabe >= 0.1.2",
         "requests >= 1.1.0",
         "IXDjango >= 0.1.1",
         "IXWSAuth >= 0.1.1",
