71f006b5fc17356559938cc0044cebcc86d98634
Alexey Kotlyarov <alexey@infoxchange.net.au>
2013-07-25 15:39:51 +1000

    Style

--- ixprofile_client/exceptions.py
+++ ixprofile_client/exceptions.py
@@ -1,31 +1,37 @@
 """
-Exceptions raised when interacting with the profile server.
+Exceptions raised when interacting with the profile server
 """
 
 
 class ProfileServerException(Exception):
     """
-    Base exception for all profile server errors.
+    Base exception for all profile server errors
     """
     pass
 
 
 class EmailNotUnique(ProfileServerException):
     """
-    An email used in an interaction with the profile server is not unique.
+    An email used in an interaction with the profile server is not unique
     """
     def __init__(self, email):
         super(EmailNotUnique, self).__init__()
         self.email = email
 
     def __str__(self):
-        "String representation of the exception."
+        """
+        String representation of the exception
+        """
         return self.__unicode__()
 
     def __unicode__(self):
-        "Unicode representation of the exception."
+        """
+        Unicode representation of the exception
+        """
         return "Email %s is not unique on the profile server." % self.email
 
     def __repr__(self):
-        "Unique representation of the exception."
+        """
+        Unique representation of the exception
+        """
         return "EmailNotUnique('%s')" % self.email
