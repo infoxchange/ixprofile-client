f5bfdf3f16444a3dc4f2615effa4a18805b3f958
Danielle Madeley <dmadeley@infoxchange.net.au>
2013-11-12 17:22:29 +1100

    Add a default login frame template that you can include into your apps
    This frame handles things like preserving the URL when you redirect and
    redirects into an unboxing page. Requires Javascript, but that's how
    the cookie crumbles.
    
    Remove the now unrequired code paths.
    
    To use in an app:
    
    LOGIN_URL = 'home'  # a page that includes login-frame.html
    LOGIN_REDIRECT_URL = '/unbox'

--- ixprofile_client/urls.py
+++ ixprofile_client/urls.py
@@ -2,13 +2,12 @@
 Route configuration for the IX Profile client
 """
 from django.conf.urls import include, patterns, url
-
-from ixprofile_client.views import login_start
+from django.views.generic import TemplateView
 
 # pylint:disable=C0103
 # The name 'urlpatterns' is a part of the API
 urlpatterns = patterns(
     '',
     url(r'', include('social_auth.urls')),
-    url(r'^login-start$', login_start, name='ixprofile-start-login'),
+    url(r'^unbox/', TemplateView.as_view(template_name='unbox.html')),
 )
