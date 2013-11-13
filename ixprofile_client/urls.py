6d0311cb8bb282a9ccd256dd65421a4c4196c89d
Danielle Madeley <dmadeley@infoxchange.net.au>
2013-11-13 11:27:07 +1100

    Set 'next' to either the outer frame next or LOGIN_REDIRECT_URL
    Move unbox to ixlogin so that it's out of the way of the main namespace.

--- ixprofile_client/urls.py
+++ ixprofile_client/urls.py
@@ -1,6 +1,7 @@
 """
 Route configuration for the IX Profile client
 """
+
 from django.conf.urls import include, patterns, url
 from django.views.generic import TemplateView
 
@@ -9,5 +10,5 @@ from django.views.generic import TemplateView
 urlpatterns = patterns(
     '',
     url(r'', include('social_auth.urls')),
-    url(r'^unbox/', TemplateView.as_view(template_name='unbox.html')),
+    url(r'^ixlogin/unbox/', TemplateView.as_view(template_name='unbox.html')),
 )
