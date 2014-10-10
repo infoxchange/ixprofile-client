5cc53b19b30a2a75bf73ca9083a08c70a9b4f0a7
Alex Hutton <ahutton@infoxchange.net.au>
2014-10-10 20:56:08 +1100

    Pep8

--- ixprofile_client/tests/test_steps.py
+++ ixprofile_client/tests/test_steps.py
@@ -32,11 +32,11 @@ FEATURE1 = """
 Feature: Test fake profile server
   Scenario: Initialize profile server
     Given I have users in the fake profile server:
-      | email           | subscribed | first_name | last_name | phone      | subscriptions          |
-      | zoidberg@px.ea  | true       | John       | Zoidberg  | 1468023579 |                        |
-      | hmcdoogal@px.ea | false      | Hattie     | McDoogal  |            |                        |
-      | acalculon@px.ea | true       | Antonio    | Calculon  | 0292538800 |                        |
-      | mendoza@mcog.fr | true       | Mendoza    | Unknown   |            | golden-condor,solaris  |
+      | email           | subscribed | first_name | last_name | phone      | subscriptions          | # nopep8
+      | zoidberg@px.ea  | true       | John       | Zoidberg  | 1468023579 |                        | # nopep8
+      | hmcdoogal@px.ea | false      | Hattie     | McDoogal  |            |                        | # nopep8
+      | acalculon@px.ea | true       | Antonio    | Calculon  | 0292538800 |                        | # nopep8
+      | mendoza@mcog.fr | true       | Mendoza    | Unknown   |            | golden-condor,solaris  | # nopep8
 """
 
 
@@ -77,7 +77,7 @@ class TestLettuceSteps(object):
             'last_name': u'Calculon',
             'phone': u'0292538800',
         },
-        u'mendoza@mcog.fr' : {
+        u'mendoza@mcog.fr': {
             'subscribed': True,
             'first_name': u'Mendoza',
             'last_name': u'Unknown',
