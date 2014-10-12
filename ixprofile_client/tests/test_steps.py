9c61c7ed281910e12d78818ef91adcddacb8cd37
Alex Hutton <ahutton@infoxchange.net.au>
2014-10-13 10:18:46 +1100

    Moved feature into separate file

--- ixprofile_client/tests/test_steps.py
+++ ixprofile_client/tests/test_steps.py
@@ -3,6 +3,7 @@ Test Lettuce Steps
 """
 
 import django
+import os
 
 from django.conf import settings
 from lettuce.core import Feature
@@ -27,17 +28,9 @@ from ixprofile_client import webservice
 # pylint:disable=invalid-name
 original_profile_server = webservice.profile_server
 
-
-FEATURE1 = """
-Feature: Test fake profile server
-  Scenario: Initialize profile server
-    Given I have users in the fake profile server:
-      | email           | subscribed | first_name | last_name | phone      | subscriptions          | # nopep8
-      | zoidberg@px.ea  | true       | John       | Zoidberg  | 1468023579 |                        | # nopep8
-      | hmcdoogal@px.ea | false      | Hattie     | McDoogal  |            |                        | # nopep8
-      | acalculon@px.ea | true       | Antonio    | Calculon  | 0292538800 |                        | # nopep8
-      | mendoza@mcog.fr | true       | Mendoza    | Unknown   |            | golden-condor,solaris  | # nopep8
-"""
+feature1_filename = os.path.join(os.path.dirname(__file__), 'FEATURE1.feature')
+with open(feature1_filename) as feature1_file:
+    FEATURE1 = feature1_file.read()
 
 
 class TestLettuceSteps(object):
