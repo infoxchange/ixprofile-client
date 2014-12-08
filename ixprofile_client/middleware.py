06a76a94c3476337d129fe0784a41005724c8777
Alex Hutton <ahutton@infoxchange.net.au>
2014-12-08 16:03:04 +1100

    Removed the check for DEBUG.
    This means the message will print in all environments, not only
    staging and prod.
    Extended the docstring.

--- ixprofile_client/middleware.py
+++ ixprofile_client/middleware.py
@@ -3,7 +3,6 @@ Middleware specific to ixprofile_client
 """
 
 from ixprofile_client.exceptions import EmailNotUnique
-from django.conf import settings
 from django.template.response import SimpleTemplateResponse
 
 
@@ -18,6 +17,9 @@ class PrintEmailNotUniqueMessage(object):
         Prints the message for the EmailNotUnique Error.
         Return HttpResponse prevents any other middleware in
         the chain from running.
+        If you wish to do something other than display this message,
+        you can catch EmailNotUnique before process_exception() is
+        invoked.
         """
-        if isinstance(exception, EmailNotUnique) and not settings.DEBUG:
+        if isinstance(exception, EmailNotUnique):
             return SimpleTemplateResponse('email_not_unique.html')
