57e4ed490d22f15227c9748dc21c9299f1b98018
Alexey Kotlyarov <alexey@infoxchange.net.au>
2014-12-12 12:11:31 +1100

    Configure settings in AppConfig
    [#48008]

--- ixprofile_client/__init__.py
+++ ixprofile_client/__init__.py
@@ -1,3 +1,35 @@
 """
 A django-socialauth based client for the IX Profile server
 """
+
+from django.apps import AppConfig
+from django.conf import settings
+
+
+class IXProfileClientConfig(AppConfig):
+    """
+    Application configuration for the IX Profile client.
+    """
+
+    name = 'ixprofile_client'
+
+    def ready(self):
+        """
+        Configure the social auth pipeline.
+        """
+
+        settings.SOCIAL_AUTH_PIPELINE = (
+            'social.pipeline.social_auth.social_details',
+            'social.pipeline.social_auth.social_uid',
+            'social.pipeline.social_auth.social_user',
+            'ixprofile_client.pipeline.match_user',
+            'social.pipeline.user.create_user',
+            'social.pipeline.social_auth.associate_user',
+            'social.pipeline.social_auth.load_extra_data',
+            'social.pipeline.user.user_details',
+        )
+
+
+# pylint:disable=invalid-name
+default_app_config = 'ixprofile_client.IXProfileClientConfig'
+# pylint:enable=invalid-name
--- ixprofile_client/backends.py
+++ ixprofile_client/backends.py
@@ -10,24 +10,6 @@ from django.core.exceptions import ImproperlyConfigured
 from social.backends import open_id
 
 
-settings.SOCIAL_AUTH_PIPELINE = (
-    'social.pipeline.social_auth.social_details',
-    'social.pipeline.social_auth.social_uid',
-    'social.pipeline.social_auth.social_user',
-    'ixprofile_client.pipeline.match_user',
-    'social.pipeline.user.create_user',
-    'social.pipeline.social_auth.associate_user',
-    'social.pipeline.social_auth.load_extra_data',
-    'social.pipeline.user.user_details',
-)
-
-
-if not settings.PROFILE_SERVER:
-    raise ImproperlyConfigured(
-        "No profile server URL (PROFILE_SERVER) in settings."
-    )
-
-
 class IXProfile(open_id.OpenIdAuth):
     """
     A backend for authentication via the IX Profile server
@@ -42,4 +24,11 @@ class IXProfile(open_id.OpenIdAuth):
         mocking of settings.PROFILE_SERVER.
         """
 
-        return urljoin(settings.PROFILE_SERVER, '/id/xrds/')
+        try:
+            profile_server = settings.PROFILE_SERVER
+        except AttributeError:
+            raise ImproperlyConfigured(
+                "No profile server URL (PROFILE_SERVER) in settings."
+            )
+
+        return urljoin(profile_server, '/id/xrds/')
