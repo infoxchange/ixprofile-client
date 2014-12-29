28064dc0e9b56aff8fbf33256ba2f03c387c74e5
Alexey Kotlyarov <alexey@infoxchange.net.au>
2014-12-29 16:33:17 +1100

    Supply SSL_CA_FILE when validating OpenID server SSL certificate

--- ixprofile_client/docker_settings.py
+++ ixprofile_client/docker_settings.py
@@ -2,9 +2,13 @@
 Standard config for loading profiles server settings from Docker
 """
 
+import inspect
+import urllib2
 import os
 
 from furl import furl
+
+from openid.fetchers import Urllib2Fetcher
 
 PROFILE_SERVER = None
 PROFILE_SERVER_KEY = None
@@ -19,3 +23,28 @@ if 'PROFILE_SERVER_URL' in os.environ:
 
     _profile_server_url.remove(username=True, password=True)
     PROFILE_SERVER = _profile_server_url.url
+
+
+class SettingsAwareFetcher(Urllib2Fetcher):
+    """
+    An URL fetcher for python-openid to verify the certificates against
+    SSL_CA_FILE in Django settings.
+    """
+
+    @staticmethod
+    def urlopen(*args, **kwargs):
+        """
+        Provide urlopen with the trusted certificate path.
+        """
+
+        # Old versions of urllib2 cannot verify certificates
+        if 'cafile' in inspect.getargspec(urllib2.urlopen).args:
+            from django.conf import settings
+            kwargs['cafile'] = settings.SSL_CA_FILE
+
+        return urllib2.urlopen(*args, **kwargs)
+
+
+# Make OpenID module trust the proper certificates
+from openid.fetchers import setDefaultFetcher
+setDefaultFetcher(SettingsAwareFetcher())
