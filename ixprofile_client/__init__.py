4d54a9c1dacb6fe65726167eeed0fee40243c1d2
Kane Cavanagh <kcavanagh@infoxchange.org>
2015-08-14 15:01:38 +1000

    Add Django 1.6 compatibility

--- ixprofile_client/__init__.py
+++ ixprofile_client/__init__.py
@@ -2,8 +2,24 @@
 A django-socialauth based client for the IX Profile server
 """
 
-from django.apps import AppConfig
 from django.conf import settings
+
+SOCIAL_AUTH_PIPELINE = (
+    'social.pipeline.social_auth.social_details',
+    'social.pipeline.social_auth.social_uid',
+    'social.pipeline.social_auth.social_user',
+    'ixprofile_client.pipeline.match_user',
+    'social.pipeline.user.create_user',
+    'social.pipeline.social_auth.associate_user',
+    'social.pipeline.social_auth.load_extra_data',
+    'social.pipeline.user.user_details',
+)
+
+try:
+    # Django 1.7+
+    from django.apps import AppConfig
+except ImportError:
+    settings.SOCIAL_AUTH_PIPELINE = SOCIAL_AUTH_PIPELINE
 
 
 class IXProfileClientConfig(AppConfig):
@@ -18,16 +34,7 @@ class IXProfileClientConfig(AppConfig):
         Configure the social auth pipeline.
         """
 
-        settings.SOCIAL_AUTH_PIPELINE = (
-            'social.pipeline.social_auth.social_details',
-            'social.pipeline.social_auth.social_uid',
-            'social.pipeline.social_auth.social_user',
-            'ixprofile_client.pipeline.match_user',
-            'social.pipeline.user.create_user',
-            'social.pipeline.social_auth.associate_user',
-            'social.pipeline.social_auth.load_extra_data',
-            'social.pipeline.user.user_details',
-        )
+        settings.SOCIAL_AUTH_PIPELINE = SOCIAL_AUTH_PIPELINE
 
 
 # pylint:disable=invalid-name
