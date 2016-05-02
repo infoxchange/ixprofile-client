c8a90610a3f75e9e46e076ecbb097f2aec494129
Kane <kcavanagh@infoxchange.org>
2016-05-02 14:26:37 +1000

    Bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt') as requirements, \
         open('test_requirements.txt') as test_requirements:
     setup(
         name='IXProfileClient',
-        version='0.2.0',
+        version='1.0.0',
         author='Infoxchange dev team',
         author_email='devs@infoxchange.org',
         packages=find_packages(),
